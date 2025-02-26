9a8261475400452340a799abdb819baa472bc9bf
Raj Ramesh Bengade <157134976+rajbengade07@users.noreply.github.com>
2025-02-26 22:52:16 +0530

    Add files via upload

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,1 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
 
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img className='logo' src={assets.logo} alt="" />
+      {/* <img className='profile' src={assets.profile_image} alt="" /> */}
+      <img className='profile' src="https://cdn-icons-png.flaticon.com/128/2202/2202112.png" alt=''/>
+    </div>
+  )
+}
+
+export default Navbar
