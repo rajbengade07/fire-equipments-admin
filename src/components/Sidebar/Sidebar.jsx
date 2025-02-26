c78beadb099a3d6f787b39372c966cadca87a470
Raj Ramesh Bengade <157134976+rajbengade07@users.noreply.github.com>
2025-02-26 22:53:59 +0530

    Add files via upload

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,1 +1,16 @@
+import React from 'react'
+import  './Sidebar.css'
+import { assets } from '../../assets/assets'
+import { NavLink } from 'react-router-dom'
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import Order from '../../pages/Orders/Orders';
+import List from '../../pages/List/List';
 
+
+const Sidebar = () => {
+  return (
+   <div className=""></div>
+  )
+}
+
+export default Sidebar
